134d3510fdf06e76ff2e1214fe2818761fdc67f5
YasminSLobo <125031166+YasminLobo@users.noreply.github.com>
2024-06-10 15:19:51 -0300

    Add files via upload

---
+++ Exercicios(Fundamentais)/exercicio10/ex10.js
@@ -0,0 +1,17 @@
+function openModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+// Fechar o modal clicando fora da caixa
+window.onclick = function(event) {
+    var modal = document.getElementById("modal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
